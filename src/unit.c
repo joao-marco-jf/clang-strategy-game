1872f725fdeeec94313e9dc8f9942acc9588172e
joao-marco-jf <joaomarcos00512@gmail.com>
2024-05-30 14:59:25 -0300

    Implementação da função de alocação de 'unit'

--- src/unit.c
+++ src/unit.c
@@ -1,7 +1,15 @@
+#include <stdlib.h>
 #include "unit.h"
 
 unit_t *allocate_unit(int x, int y, unit_e type){
-    
+    unit_t *new_unit = NULL;
+    new_unit = (unit_t *) malloc(sizeof(unit_t));
+    if(new_unit == NULL) return;
+    new_unit->x = x;
+    new_unit->y = y;
+    new_unit->type = type;
+    new_unit->next = NULL;
+    return new_unit;
 }
 
 void insert_unit(unit_t **units, int x, int y, unit_e type){

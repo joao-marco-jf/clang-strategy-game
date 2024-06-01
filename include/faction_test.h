5cfc77033ff22a65e05456a0164e1c219bf119b4
joao-marco-jf <joaomarcos00512@gmail.com>
2024-06-01 16:43:55 -0300

    Implementação de testes de 'faction'

---
+++ include/faction_test.h
@@ -0,0 +1,8 @@
+#ifndef FACTION_TEST_H
+#define FACTION_TEST_H
+
+void allocate_faction_test();
+void insert_faction_test();
+void remove_faction_test();
+
+#endif
--- tests/main_test.c
+++ tests/main_test.c
@@ -2,6 +2,7 @@
 
 #include "unit_test.h"
 #include "building_test.h"
+#include "faction_test.h"
 
 int main() {
   if (CUE_SUCCESS != CU_initialize_registry())
@@ -53,6 +54,25 @@ int main() {
   }
 
   /* -------------------------- BUILDING_TEST --------------------------*/
+
+  /* -------------------------- FACTION_TEST --------------------------*/
+
+  if (NULL == CU_add_test(pSuite, "allocate_faction_test", allocate_faction_test)) {
+    CU_cleanup_registry();
+    return CU_get_error();
+  }
+
+  if (NULL == CU_add_test(pSuite, "insert_faction_test", insert_faction_test)) {
+    CU_cleanup_registry();
+    return CU_get_error();
+  }
+
+  if (NULL == CU_add_test(pSuite, "remove_faction_test", remove_faction_test)) {
+    CU_cleanup_registry();
+    return CU_get_error();
+  }
+
+  /* -------------------------- FACTION_TEST --------------------------*/
 
    CU_basic_set_mode(CU_BRM_VERBOSE);
    CU_basic_run_tests();

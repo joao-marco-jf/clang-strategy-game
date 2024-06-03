c5c685b8f1c2ea7ef74de7fb4ee0e4b3b36ae37b
joao-marco-jf <joaomarcos00512@gmail.com>
2024-06-03 11:59:54 -0300

    Implementação de inserção de 'building'

--- src/building.c
+++ src/building.c
@@ -13,7 +13,11 @@ building_t *allocate_building(int x, int y, building_e type){
 }
 
 void insert_building(building_t **buildings, int x, int y, building_e type){
-
+    building_t *new_building = NULL;
+    new_building = allocate_building(x, y, type);
+    if(new_building == NULL) return;
+    new_building->next = *buildings;
+    *buildings = new_building;
 }
 
 void remove_building(building_t **buildings, int x, int y){

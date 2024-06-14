d75ef231ca846f2b9220a0303d06fdd98b3f4e6e
CauaRodrigues05 <caua05rodrigues@gmail.com>
2024-06-14 14:18:58 -0300

    Balance

--- include/game/board.h
+++ include/game/board.h
@@ -1,10 +1,9 @@
-#include "faction.h"
-#include "building.h"
-#include "unit.h"
-
 #ifndef BOARD_H
 #define BOARD_H
 
+#include "faction.h"
+#include "building.h"
+#include "unit.h"
 
 typedef struct board_poss_t
 {
@@ -18,3 +17,5 @@ typedef struct board_poss_t
 }board_poss_t;
 
 board_poss_t *allocate_board(int line, int column, unit_t *p_unit_t, building_t *p_building_t, faction_t *p_faction_t);
+
+#endif

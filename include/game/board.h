3edef332b3bd1c161463cc3905be83604a7711a1
joao-marco-jf <joaomarcos00512@gmail.com>
2024-06-15 13:45:09 -0300

    Alteração da struct 'board_t'

--- include/game/board.h
+++ include/game/board.h
@@ -5,17 +5,18 @@
 #include "building.h"
 #include "unit.h"
 
-typedef struct board_poss_t
-{
-    int line;
-    int column;
+typedef struct board_t{
     unit_t* p_unit_t;
     building_t* p_building_t;
     faction_t* p_faction_t;
-    struct board_poss_t *right;
-    struct board_poss_t *bellow;
-}board_poss_t;
+}board_t;
 
-board_poss_t *allocate_board(int line, int column, unit_t *p_unit_t, building_t *p_building_t, faction_t *p_faction_t);
+void init_board(int lines, int columns, board_t board[lines][columns]);
+void print_board(int lines, int columns, board_t board[lines][columns]);
+void insert_faction_on_board(int lines, int columns, board_t board[columns][lines], faction_t** factions, char name[15], int x, int y);
+void insert_building_on_board(int lines, int columns, board_t board[columns][lines], building_t** buildings, int x, int y, building_e type);
+void insert_unit_on_board(int lines, int columns, board_t board[columns][lines], unit_t** units, int x, int y, unit_e type);
+
+void remove_unit_on_board(int lines, int columns, board_t board[columns][lines], unit_t** units, int x, int y);
 
 #endif

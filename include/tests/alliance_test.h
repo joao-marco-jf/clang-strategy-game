401eefa2833bdcc7e334809554e42f86980e9f24
joao-marco-jf <joaomarcos00512@gmail.com>
2024-06-16 20:44:41 -0300

    Remoção de testes inutilizados

--- include/tests/alliance_test.h
+++
@@ -1,8 +0,0 @@
-#ifndef ALLIANCE_TEST_H
-#define ALLIANCE_TEST_H
-
-void allocate_alliance_test();
-void insert_alliance_test();
-void remove_alliance_test();
-
-#endif
--- include/tests/building_test.h
+++
@@ -1,8 +0,0 @@
-#ifndef BUILDING_TEST_H
-#define BUILDING_TEST_H
-
-void allocate_building_test();
-void insert_building_test();
-void remove_building_test();
-
-#endif

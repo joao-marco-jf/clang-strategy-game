9bc6dc229e535a598de0fd4ba6ecef5e93a008b7
joao-marco-jf <joaomarcos00512@gmail.com>
2024-06-17 18:45:58 -0300

    Implementação de possibilidade para alocar mais de uma únida em um mesmo nó, limitado a 3 unidades.

--- include/board.h
+++ include/board.h
@@ -13,6 +13,8 @@ typedef struct node_t{
     int line;
     int col;
     unit_t *unit;
+    unit_t *unit1;
+    unit_t *unit2;
     building_t *building;
     faction_t *faction;
     struct node_t *next;
@@ -25,11 +27,13 @@ typedef struct board_t{
 }board_t;
 
 board_t *create_board(int lines, int columns);
-node_t *allocate_node(int line, int col, unit_t* p_unit, building_t* p_building, faction_t* p_faction);
-void insert_node(board_t *board, int line, int col, unit_t *p_unit, building_t *p_building, faction_t *p_factions);
+node_t *allocate_node(int line, int col, unit_t* unit, building_t* building, faction_t* faction);
+void insert_node(board_t *board, int line, int col, unit_t *unit, building_t *building, faction_t *faction);
 faction_t *get_faction_board(board_t *board, int line, int col);
 building_t *get_building_board(board_t *board, int line, int col);
 unit_t *get_unit_board(board_t *board, int line, int col);
+unit_t *get_unit1_board(board_t *board, int line, int col);
+unit_t *get_unit2_board(board_t *board, int line, int col);
 void remove_node(board_t *board_t, int row, int col);
 void free_board(board_t *board);
 void print_board(board_t *board);

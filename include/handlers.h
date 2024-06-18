b645e0183d7ace7ffae461da146449fe3cc8cddc
joao-marco-jf <joaomarcos00512@gmail.com>
2024-06-17 23:15:00 -0300

    Atualização de cabeçalhos 'handlers.h'

--- include/handlers.h
+++ include/handlers.h
@@ -10,15 +10,15 @@
 #define MAX_PART_LEN 15
 
 // Handlers
-void handle_alliance(faction_t **factions, char *part, char *faction);
-void handle_attack(faction_t **factions, char *part, char *param);
-void handle_combat(char *part, char *enemy_name, int self_value, int enemy_value);
-void handle_position_faction(board_t **board, faction_t **factions, char *part, int *params);
-void handle_position_unit(board_t **board, faction_t **factions, unit_t **units, char *part, int *params);
-void handle_move(board_t *board, unit_t **units, char part[MAX_PART_LEN], int *params);
-void handle_collect(faction_t **factions, char part[MAX_PART_LEN]);
-void handle_building(board_t **board, faction_t **factions, building_t **buildings, char *part, int *params);
-void handle_defend(faction_t **factions, char part[MAX_PART_LEN]);
-void handle_earn(faction_t **factions, char faction_name[MAX_PART_LEN], int power);
+void handle_alliance(FILE *log, faction_t **factions, char *part, char *faction);
+void handle_attack(FILE *log, faction_t **factions, char *part, char *param);
+void handle_combat(FILE *log, board_t *board, unit_t **units, char *part, char *enemy_name);
+void handle_position_faction(FILE *log, board_t **board, faction_t **factions, char *part, int *params);
+void handle_position_unit(FILE *log, board_t **board, faction_t **factions, unit_t **units, char *part, int *params);
+void handle_move(FILE *log, board_t *board, unit_t **units, char part[MAX_PART_LEN], int *params);
+void handle_collect(FILE *log, faction_t **factions, unit_t **units, int columns, int rows, int map[columns][rows], char part[MAX_PART_LEN]);
+void handle_building(FILE *log, board_t **board, faction_t **factions, building_t **buildings, char *part, int *params);
+void handle_defend(FILE *log, faction_t **factions, char part[MAX_PART_LEN]);
+void handle_earn(FILE *log, faction_t **factions, char faction_name[MAX_PART_LEN], int power);
 
 #endif // HANDLERS_H

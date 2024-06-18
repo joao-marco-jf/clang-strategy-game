cb285ddcb8db4b13b22f35b3ab83b0560801982c
CauaRodrigues05 <caua05rodrigues@gmail.com>
2024-06-17 23:13:23 -0300

    Documentação da faction.c

--- src/faction.c
+++ src/faction.c
@@ -42,21 +42,45 @@ void insert_faction(faction_t **factions, char name[15], int resources, int powe
     *factions = new_faction;
 }
 
+/**
+ * @brief Busca uma facção na lista encadeada de facções pelo nome.
+ *
+ * A função `get_faction` percorre a lista encadeada de facções (`faction_t`) apontada
+ * por `*factions`, buscando por uma facção cujo nome seja igual ao nome especificado
+ * pelo parâmetro `name`. Se encontrar a facção, retorna um ponteiro para ela. Caso contrário,
+ * retorna NULL.
+ *
+ * @param factions Ponteiro para o ponteiro da lista encadeada de facções.
+ * @param name Nome da facção a ser buscada.
+ * 
+ * @return Retorna um ponteiro para a facção encontrada ou NULL se não encontrada.
+ */
 faction_t *get_faction(faction_t **factions, char name[2]){
-    faction_t *current = *factions;
+    faction_t *current = *factions; // Inicializa o ponteiro `current` com o início da lista
     while(current != NULL){
+        // Compara o nome da facção atual com `name`
         if(strcmp(current->name, name) == 0){
-            return current;
+            return current; // Retorna a facção atual se o nome corresponder
         }
-        current = current->next;
+        current = current->next; // Avança para o próximo nó da lista encadeada
     }
-    return NULL;
+    return NULL; // Retorna NULL se a facção não for encontrada
 }
 
+/**
+ * @brief Libera a memória alocada para todos os nós de uma lista encadeada de facções.
+ *
+ * A função `free_factions` percorre a lista encadeada de facções (`faction_t`) apontada
+ * por `*factions` e libera a memória alocada para cada nó. Após a liberação de cada nó,
+ * o ponteiro do início da lista (`*factions`) é atualizado para apontar para o próximo nó.
+ * Quando todos os nós são liberados, o ponteiro é definido como NULL.
+ *
+ * @param factions Ponteiro para o ponteiro da lista encadeada de facções.
+ */
 void free_factions(faction_t **factions){
     while(*factions != NULL){
-        faction_t *temp = *factions;
-        *factions = (*factions)->next;
-        free(temp);
+        faction_t *temp = *factions;    // Armazena o nó atual em `temp`
+        *factions = (*factions)->next;  // Atualiza `*factions` para apontar para o próximo nó
+        free(temp);                     // Libera a memória alocada para o nó atual
     }
 }
